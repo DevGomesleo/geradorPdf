df72fe814d77151955b4802e80a6d5aad23f2b19
Leo Gomes <devleogomes@outlook.com>
2023-06-13 16:23:03 -0300

    modified:   css/styles.css 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 
 function gerarPdf(){
-    alert('ok')
+
+    const conteudo = document.querySelector('.seuTexto').value;
+    //Opções para o Doc
+    const options ={ 
+        margin: [10,10,10,10],
+        filename: "arquivo.pdf",
+        html2canvas : {scale:3 , width:750},
+        jsPDF: {unit: "mm", format:"a4", orientation : "portrait"}
+    }
+
+
+
+    // Criar e Baixar PDF
+    html2pdf().set(options).from(conteudo).save();
 }
